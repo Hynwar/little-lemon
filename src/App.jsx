f417b47a0ae2a10078f8ac83029b91ac3c2d5949
Hiba Anwar <hiba2anwar@gmail.com>
2023-09-03 21:03:51 +0400

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Hero />
 		</>
 	);
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
+import heroImg from '../assets/hero-img.jpg';
+
 function Hero() {
 	return (
-		<section>
-			<h1>Little Lemon</h1>
-			<p className='subtitle'>Chicago</p>
-			<p className='description-text'>
-				We are a family owned Mediterranean restaurant, focused on traditional
-				recipes served with a modern twist.
-			</p>
-			<button className='primary-btn'>Reserve a Table</button>
+		<section id='hero-section'>
+			<div className='hero-content'>
+				<h1>Little Lemon</h1>
+				<h3 className='subtitle'>Chicago</h3>
+				<p className='description-text'>
+					We are a family owned Mediterranean restaurant, focused on traditional
+					recipes served with a modern twist.
+				</p>
+				<button className='primary-btn'>Reserve a Table</button>
+			</div>
+			<img src={heroImg} alt='Food' className='hero-img' />
 		</section>
 	);
 }
