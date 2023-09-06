98f7d7c8d15b7fb18f7737e6ac9c79465d522ec0
Hiba Anwar <hiba2anwar@gmail.com>
2023-09-06 15:34:56 +0400

    ADDED: GitHub pages and actions

--- src/components/reservation/ReservationPage.jsx
+++ src/components/reservation/ReservationPage.jsx
@@ -1,6 +1,28 @@
 import '../../styles/ReservationPage.css';
 
 function ReservationPage() {
-	return <div>ReservationPage</div>;
+	return (
+		<div className='reservation-info'>
+			<h1>Table Reservation</h1>
+			<h4>Opening Hours</h4>
+			<p className='opening-info'>
+				Monday - Friday: 8AM to 10PM
+				<br />
+				<br />
+				Saturday & Sunday: 9AM to 12PM
+			</p>
+
+			<h5>Reservation Policy</h5>
+			<ul>
+				<li>We accept reservations for both lunch and dinner services.</li>
+				<li>
+					For same-day reservations, please call us directly at (312)-593-2744.
+				</li>
+				<li>
+					Walk-ins are welcome and will be accommodated based on availability.
+				</li>
+			</ul>
+		</div>
+	);
 }
 export default ReservationPage;
